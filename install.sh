d61d7feda43eae250a9c8e684118bd44718c6082
Austen Sutherland <sutherlandausten@gmail.com>
2026-03-19 15:49:53 -0400

    Set to current branch to test one liner

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-opencode --prompt "install https://raw.githubusercontent.com/superultrainc/opencode-superwhisper/main/prompt.txt" </dev/tty
+opencode --prompt "install https://raw.githubusercontent.com/superultrainc/opencode-superwhisper/update/install-script/prompt.txt"
